9e14c591c868805774597c4dcf65f7a0125bf652
sajin sha <124073331+sajinsha46@users.noreply.github.com>
2023-10-27 14:11:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {HashRouter, Routes, Route} from 'react-router-dom';
+import './components/style.css'
+import Navbar from './components/Navbar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Home , About, Info, Contact, Project} from './components/Navcomponents';
+
+function App() {
+  return (
+    <div>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Project />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
